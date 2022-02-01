9882cf422ce2d8138bd67d9a8632c0c0c9f2397b
chagy <chagy_x_@hotmail.com>
2022-02-01 10:09:03 +0700

    9

--- playground.ts
+++ playground.ts
@@ -1,5 +1,19 @@
 
 
 export default function play(){
-    console.log("Hellow World")
+    // console.log("Hellow World")
+    const name: string = "Filip"
+    const age: number = 30
+
+    function logPersonInfo(personName: string,personAge: number){
+        // const info = "Name: "+personName+", age: "+personAge
+        const info = `Name: ${personName}, age: ${personAge}`
+        console.log(info)
+        return info
+    }
+
+    logPersonInfo(name,age)
+    // logPersonInfo(true,false)
+    // logPersonInfo(12,12)
+    // logPersonInfo(null,undefined)
 }
