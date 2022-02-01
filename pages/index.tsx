a6e4cd1270c608edb70884ae67356aafa4d5c583
chagy <chagy_x_@hotmail.com>
2022-02-01 09:45:27 +0700

    7

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,15 @@ export default function Home() {
 
   const message: string = "Hello World"
   let age: number = 30
+
   let a: AddEventListenerOptions;
+  let b: NodeJS.Process
+
+  let person: Person = {
+    name: 'Filip'
+  }
+
+  console.log(b.browser)
 
   return (
     <div>
