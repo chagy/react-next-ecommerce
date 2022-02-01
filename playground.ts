1c4c6a58d557241bc0f9fc1a17ba56c740c1b1aa
chagy <chagy_x_@hotmail.com>
2022-02-01 10:17:17 +0700

    10

--- playground.ts
+++ playground.ts
@@ -1,9 +1,23 @@
 
+interface Person {
+        name: string,
+        age: number
+    }
+
+// type Person = {
+//     name: string,
+//     age: number
+// }
 
 export default function play(){
     // console.log("Hellow World")
     const name: string = "Filip"
     const age: number = 30
+
+    const person: Person = {
+        name: "John",
+        age: 34
+    }
 
     function logPersonInfo(personName: string,personAge: number){
         // const info = "Name: "+personName+", age: "+personAge
@@ -12,8 +26,16 @@ export default function play(){
         return info
     }
 
+    function logPersonInfo2(person: Person){
+        // const info = "Name: "+personName+", age: "+personAge
+        const info = `Name: ${person.name}, age: ${person.age}`
+        console.log(info)
+        return info
+    }
+
     logPersonInfo(name,age)
     // logPersonInfo(true,false)
     // logPersonInfo(12,12)
     // logPersonInfo(null,undefined)
+    logPersonInfo2(person)
 }
