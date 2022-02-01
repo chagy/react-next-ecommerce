441e94b8aa0d48dc4486983e879ce1b3d1b20583
chagy <chagy_x_@hotmail.com>
2022-02-01 11:24:13 +0700

    19

--- playground.ts
+++ playground.ts
@@ -12,24 +12,21 @@ interface AcademicPerson extends Person {
     publications: string[]
 }
 
-type Car = {
-    name: string
+type RaceCar = {
+    name: string,
+    maxSpeed: number
 }
 
-type RaceCar = {
-    speed: number
-} & Car & { mileage: number }
+type CityCar = {
+    name: string,
+    space: string
+}
+
+type Car = RaceCar | CityCar
 
 export default function play() {
 
-    const car: RaceCar = {
-        name: "my car",
-        speed: 100,
-        mileage: 200,
+    function logCarInfo(car: Car) {
+        console.log(car.name)
     }
-
-    function logCar(car: Car) { }
-
-    logCar(car)
-
 }
