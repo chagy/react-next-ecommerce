9040f1cdc2de5cc6de85982e241bcfcdf5c06296
chagy <chagy_x_@hotmail.com>
2022-02-02 10:29:51 +0700

    34

--- playground.ts
+++ playground.ts
@@ -2,49 +2,42 @@
 
 interface Person {
     name: string
+}
+
+interface Student extends Person {
     age: number
-    city: string
 }
 
-class Student implements Person {
-    name = ""
-    age = 0
-    city = ""
+interface PostGraduadeStudent extends Person {
+    age: number
+    projects: string[]
 }
 
-class BussinessPerson implements Person {
-    name = ""
-    age = 0
-    city = ""
-    salary = 1000
-}
+// type StudentInfo<T extends Student = Student> = {
+//     data: T
+//     grades: number[]
+// }
 
-type Car = {
-    brand: string,
-    age: number,
-    name: string,
-    city: string
-}
-
-class Logger<T extends Person = Car> {
-    log(items: Array<T>, callback: (i: T) => void) {
-        items.forEach((item) => {
-            callback(item)
-        })
-    }
+type StudentInfo<T extends Student = Student> = {
+    data: T
+    grades: number[]
 }
 
 export default function play() {
 
-    const logger = new Logger()
+    function logStudentInfo(info: StudentInfo<PostGraduadeStudent>) {
+        console.log(info.data.name)
+        console.log(info.data.age)
+        console.log(info.data.projects)
+    }
 
-    const persons = [
-        { name: 'filip', age: 30, city: "", brand: "" },
-        { name: 'john', age: 25, city: "", brand: "" }
-    ]
+    const info = {
+        data: {
+            name: "Filip",
+            age: 30,
+        },
+        grades: [1, 2, 3, 1]
+    }
 
-    logger.log(persons, (person) => {
-        console.log(person)
-    })
-
+    logStudentInfo(info)
 }
