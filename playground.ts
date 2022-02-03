e42a44fcee6c9744709ddbf3e4ecfbb8378af2bb
chagy <chagy_x_@hotmail.com>
2022-02-03 09:22:37 +0700

    43

--- playground.ts
+++ playground.ts
@@ -4,10 +4,16 @@ interface Person {
     grades: number[]
 }
 
-type PersonKey = keyof Person
+type Logger<FP = string, SP = number, RT = string> = (param1: FP, param2: SP) => RT
 
 export default function play() {
 
-    const personKey: keyof Person = "grades"
+    const superLogger: Logger<string, number, string> = (name, age) => {
+        return "Hello World"
+    }
+
+    const logger: Logger = (brand, age) => {
+        return brand + age
+    }
 
 }
