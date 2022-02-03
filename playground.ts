4b8cd905eab9315c0733350a63c565f6a5b4631e
chagy <chagy_x_@hotmail.com>
2022-02-03 08:37:56 +0700

    39

--- playground.ts
+++ playground.ts
@@ -6,8 +6,18 @@ type CustomArray<T> = {
     [index: number]: T
 }
 
+type CustomObject<T = string | number | Person> = {
+    [key: string]: T
+}
+
 export default function play() {
 
-    const items: CustomArray<string> = ["1", "2", "3"]
-    const items2: CustomArray<number> = [1, 2, 3]
+    const person: CustomObject = {
+        "age": 23,
+        "name": "ad",
+        "city": "add",
+        person: {
+            name: "Filip"
+        }
+    }
 }
