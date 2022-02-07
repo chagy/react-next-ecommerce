84f24f6493495f7b3ebb53340b1b7bd6e8ebf9f0
chagy <chagy_x_@hotmail.com>
2022-02-07 10:45:30 +0700

    69

--- framework/shopify/api/config.ts
+++ framework/shopify/api/config.ts
@@ -1,4 +1,4 @@
-
+import { ApiConfig } from "@common/types/api"
 import { fetchApi } from "../utils";
 
 class Config {
--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,15 +1,16 @@
-import { fetchApi, normalizeProduct, getAllProductQuery } from "../utils"
+import { normalizeProduct, getAllProductQuery } from "../utils"
 
 
 import { ProductConnection } from '../schema';
 import { Product } from "@common/types/product"
+import { ApiConfig } from '@common/types/api';
 
 type ReturnType = {
     products: ProductConnection
 }
 
-const getAllProducts = async (): Promise<Product[]> => {
-    const { data } = await fetchApi<ReturnType>({ query: getAllProductQuery })
+const getAllProducts = async (config: ApiConfig): Promise<Product[]> => {
+    const { data } = await config.fetch<ReturnType>({ url: config.apiUrl, query: getAllProductQuery })
 
     const products = data.products.edges.map(({ node: product }) => {
         return normalizeProduct(product)
