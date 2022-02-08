df3b22f04acbf8088c58ad8dea5bce0f0d2b7548
chagy <chagy_x_@hotmail.com>
2022-02-08 09:34:11 +0700

    79

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
+import s from "./Layout.module.css";
+
 const Layout: FC = ({ children }) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <>
+      <main className={s.root}>{children}</main>
+    </>
+  );
 };
 
 export default Layout;
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 // module.exports = {
 //   plugins: [require("tailwindcss")],
 // };
+
+module.exports = {
+  plugins: ["tailwindcss", "autoprefixer"],
+};
