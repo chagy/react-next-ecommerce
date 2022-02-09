98a6d160d73e0d9ec6f2f26af6a3ea359a79ccc9
chagy <chagy_x_@hotmail.com>
2022-02-09 08:53:12 +0700

    111

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import getAllProducts from "@framework/product/get-all-products";
 import { getConfig } from "@framework/api/config";
 import { Layout } from "@components/common";
 import { ProductCard } from "@components/product";
-import { Grid, Hero } from "@components/ui";
+import { Grid, Hero, Marquee } from "@components/ui";
 
 export async function getStaticProps() {
   const config = getConfig();
@@ -32,6 +32,11 @@ export default function Home({
         headline="Cheesecake pudding liquorice jujubes"
         description="Cheesecake pudding liquorice jujubes chocolate cake tiramisu caramels ice cream. Pudding muffin tiramisu sweet roll sweet chocolate wafer. Jelly-o chocolate cake marshmallow cookie marshmallow sugar plum fruitcake topping chocolate. Halvah powder biscuit ice cream tiramisu chupa chups sweet roll jelly-o candy canes. Sesame snaps biscuit sesame snaps gummies topping. Candy pie carrot cake jelly cookie lollipop. Brownie toffee caramels tootsie roll gummi bears halvah. Chocolate bar oat cake tootsie roll cotton candy gingerbread pudding. Muffin chupa chups liquorice powder icing macaroon. Donut topping candy pastry marshmallow croissant. Lollipop donut pie ice cream danish gingerbread. Carrot cake halvah gingerbread chocolate apple pie lollipop jelly."
       />
+      <Marquee>
+        {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Marquee>
     </>
   );
 }
