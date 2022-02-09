8c70584a59ab0b409f268b0f95442933b97fd99d
chagy <chagy_x_@hotmail.com>
2022-02-09 09:57:25 +0700

    117

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import s from "./Layout.module.css";
+import { Footer } from "@components/common";
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={s.root}>
       <main className="fit">{children}</main>
+      <Footer />
     </div>
   );
 };
