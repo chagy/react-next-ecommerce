a89a0c0bb70f70d832866e11ab9587afd89ddbbf
chagy <chagy_x_@hotmail.com>
2022-02-11 09:38:40 +0700

    150

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,13 +1,14 @@
-import React, { FC, ReactNode } from "react";
+import React, { ButtonHTMLAttributes, FC, ReactNode } from "react";
 import s from "./Button.module.css";
+import cn from "classnames";
 
-interface Props {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode | ReactNode[];
 }
 
-const Button: FC<Props> = ({ children }) => {
+const Button: FC<Props> = ({ children, className, ...rest }) => {
   return (
-    <button className={s.root} type="button">
+    <button className={cn(s.root, className)} type="button" {...rest}>
       {children}
     </button>
   );
