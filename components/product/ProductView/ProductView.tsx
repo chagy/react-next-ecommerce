777c594ac37c98c1b2827a7750da8e0f1c59534d
chagy <chagy_x_@hotmail.com>
2022-02-14 10:21:04 +0700

    163

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -8,6 +8,7 @@ import { ProductSlider, Swatch } from "@components/product";
 import { Choices, getVariant } from "../helpers";
 import { useUI } from "@components/ui/context";
 import useAddItem from "@framework/cart/use-add-item";
+import { useApiProvider } from "@common";
 
 interface Props {
   product: Product;
@@ -15,6 +16,8 @@ interface Props {
 
 const ProductView: FC<Props> = ({ product }) => {
   const [choices, setChoices] = useState<Choices>({});
+  const api = useApiProvider();
+  debugger;
   const { openSidebar } = useUI();
   const addItem = useAddItem();
 
