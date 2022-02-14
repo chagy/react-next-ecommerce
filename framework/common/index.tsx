af6a153809eb54a8658c7e4fa6263eb63bf7f294
chagy <chagy_x_@hotmail.com>
2022-02-14 11:29:27 +0700

    167

--- framework/common/index.tsx
+++ framework/common/index.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext, useMemo } from "react";
-import { ApiConfig, ApiHooks } from "./types/api";
+import { ApiConfig, ApiHooks, ApiProviderContext } from "./types/api";
 
 interface ApiProviderProps {
   children: ReactNode | ReactNode[];
@@ -7,7 +7,7 @@ interface ApiProviderProps {
   hooks: ApiHooks;
 }
 
-export const ApiContext = createContext({});
+export const ApiContext = createContext<Partial<ApiProviderContext>>({});
 
 export const ApiProvider = ({ children, config, hooks }: ApiProviderProps) => {
   const coreConfig = useMemo(() => {
@@ -17,9 +17,11 @@ export const ApiProvider = ({ children, config, hooks }: ApiProviderProps) => {
     };
   }, [config.fetch, hooks]);
 
-  return <ApiContext.Provider value={config}>{children}</ApiContext.Provider>;
+  return (
+    <ApiContext.Provider value={coreConfig}>{children}</ApiContext.Provider>
+  );
 };
 
 export const useApiProvider = () => {
-  return useContext(ApiContext);
+  return useContext(ApiContext) as ApiProviderContext;
 };
