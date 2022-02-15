cffd8383a93c27623aa374a89a84bb70c8b591a6
chagy <chagy_x_@hotmail.com>
2022-02-15 11:49:31 +0700

    190

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -1,6 +1,8 @@
 import { ApiFetcher } from "@common/types/api"
 import { Checkout, CheckoutCreatePayload, Maybe } from "@framework/schema"
 import { checkoutCreateMutation } from "./mutations"
+import { SHOPIFY_CHECKOUT_ID_COOKIE, SHOPIFY_CHECKOUT_URL_COOKIE, SHOPIFY_COOKIE_EXPIRE } from '../const';
+import Cookies from "js-cookie"
 
 const createCheckout = async (
     fetch: ApiFetcher<{ checkoutCreate: CheckoutCreatePayload }>
@@ -11,6 +13,14 @@ const createCheckout = async (
 
     const { checkout } = data.checkoutCreate
     const checkoutId = checkout?.id
+
+    if (checkoutId) {
+        const options = {
+            expires: SHOPIFY_COOKIE_EXPIRE
+        }
+        Cookies.set(SHOPIFY_CHECKOUT_ID_COOKIE, checkoutId)
+        Cookies.set(SHOPIFY_CHECKOUT_URL_COOKIE, checkout?.webUrl)
+    }
 
     return checkout
 }
