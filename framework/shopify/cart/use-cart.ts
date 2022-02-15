fe474e79032ac145095e7468befd2a00e3fc14ba
chagy <chagy_x_@hotmail.com>
2022-02-15 08:22:18 +0700

    183

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -7,12 +7,13 @@ export const handler = {
         query: ""
     },
     fetcher() {
+        console.log("I AM NOT CALLED")
         return {
             data: "cart read!!"
         }
     },
-    useHook: ({ fetch }: any) => {
-        const data = fetch()
+    useHook: ({ useData }: any) => {
+        const data = useData()
         return {
             data
         }
