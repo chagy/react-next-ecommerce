a85169f37411f983701e56b375c7bfe1acd933eb
chagy <chagy_x_@hotmail.com>
2022-02-16 09:34:41 +0700

    206

--- framework/common/types/hooks.ts
+++ framework/common/types/hooks.ts
@@ -1,4 +1,6 @@
 import { ApiFetcher, ApiFetcherOptions } from "./api"
+import { SWRResponse } from "swr"
+
 export interface ApiHooks {
     cart: {
         useAddItem: MutationHook
@@ -40,12 +42,20 @@ export type MutationHook<H extends HookDescriptor = any> = {
     ): () => (input: H["fetcherInput"]) => Promise<H["data"]>
 }
 
+export type UseDataContext = {
+    swrOptions: any
+}
+
+export type UseData<Data> = (context: UseDataContext) => Data
+
 export type SWRHook<H extends HookDescriptor = any> = {
     fetcherOptions: ApiFetcherOptions,
     fetcher: HookFetcherFn<H["fetcherInput"], H["fetcherOutput"], H["data"]>
     useHook(
-        context: SWRHookContext<H["fetcherInput"], H["data"]>
-    ): Promise<H["data"]>
+        context: {
+            useData: UseData<SWRResponse<H["data"], any>>
+        }
+    ): SWRResponse<H["data"], any>
 }
 
 export type Hook = MutationHook | SWRHook
